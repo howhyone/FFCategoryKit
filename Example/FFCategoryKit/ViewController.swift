e16439c6a2de428f185d4e786bb6e2013bcf43ba
xu.zhang <xu.zhang@yintech.cn>
2021-11-04 11:12:30 +0800

    Initial commit

---
+++ Example/FFCategoryKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FFCategoryKit
+//
+//  Created by xu.zhang on 11/04/2021.
+//  Copyright (c) 2021 xu.zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
